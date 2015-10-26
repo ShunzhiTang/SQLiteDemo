9fefe7d3dd25827ea8ba1349f797b027766b3356
tangShunZhi <1046975221@qq.com>
2015-10-25 19:02:19 -0700

    init

---
+++ SQLiteDemo/SQLiteDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SQLiteDemo
+//
+//  Created by Tsz on 15/10/25.
+//  Copyright © 2015年 Tsz. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
